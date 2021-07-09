f770b0b345b68ff57c7113aa11cd34179fbcfec5
Nahuel Vautier <nahuelearning@gmail.com>
2021-07-08 23:49:27 -0300

    v155

--- app/components/Router.js
+++ app/components/Router.js
@@ -2,6 +2,7 @@ import API from "../helpers/wp_api.js";
 import { ajax } from "../helpers/ajax.js";
 import { PostCard } from "./PostCard.js";
 import { Post } from "./Post.js";
+import { SearchCard } from "./SearchCard.js";
 
 export async function Router () {
   /* Se aplica asincronía a la función para que se ejecute en primer lugar
@@ -28,12 +29,26 @@ export async function Router () {
   } else if (hash.includes("#/search")) {
     const query = localStorage.getItem("wpSearch");
 
-    if (!query) return false;
+    if (!query) {
+      d.querySelector(".loader").style.display = "none";
+      return false;
+    };
     
     await ajax({
       url: `${API.SEARCH}${query}`,
       cbSuccess: (search) => {
         console.log(search);
+        let html = "";
+
+        if (search.length === 0) {
+          html = `
+            <p class="error">No existen resultados de búsqueda para el término "${query}"</p>
+          `;
+        } else {
+          search.forEach(word => html += SearchCard(word));
+        }
+
+        $main.innerHTML = html;
       }
     });
   } else {
